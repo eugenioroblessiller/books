5849da42bdb3444f35313cbc9b05d4f7b0cee774
eugenio <eugenio@baliotech.com>
2022-10-27 09:56:35 -0500

    lazy module for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksModule) },
+  { path: '404', component: ErrorPageComponent },
+  { path: '**', redirectTo: '404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
