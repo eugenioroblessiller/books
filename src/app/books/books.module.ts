8b370d796cf9c7a54fd661045bb1c4b0730addc4
eugenio <eugenio@baliotech.com>
2022-10-27 09:30:00 -0500

    created index module

---
+++ src/app/books/books.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { IndexComponent } from './pages/index/index.component';
+
+
+
+@NgModule({
+  declarations: [
+    IndexComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BooksModule { }
