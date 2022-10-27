726f258d516958c6a5a5f5d7c348be9a3bbb9878
eugenio <eugenio@baliotech.com>
2022-10-27 09:47:00 -0500

    auth routing module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MaterialModule } from '../material/material.module';
+import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
@@ -14,7 +15,8 @@ import { RegisterComponent } from './pages/register/register.component';
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
