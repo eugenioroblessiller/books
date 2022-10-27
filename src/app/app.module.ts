31f701f1ef1af7338bab011e20d75717a73b2c71
eugenio <eugenio@baliotech.com>
2022-10-27 09:34:14 -0500

    added material angular and angular flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/pages/login/login.component';
-import { RegisterComponent } from './auth/pages/register/register.component';
-import { IndexComponent } from './books/pages/index/index.component';
 import { SearchPipe } from './pipes/search.pipe';
 
 @NgModule({
@@ -15,7 +13,8 @@ import { SearchPipe } from './pipes/search.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
