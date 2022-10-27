b853984e3750372061649713a50ef13ac0b607b1
eugenio <eugenio@baliotech.com>
2022-10-27 09:49:42 -0500

    created a home component

---
+++ src/app/books/books-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { IndexComponent } from './pages/index/index.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent, children: [
+      { path: 'books', component: IndexComponent },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BooksRoutingModule { }
--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -4,12 +4,14 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from '../material/material.module';
 import { IndexComponent } from './pages/index/index.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 
 @NgModule({
   declarations: [
     IndexComponent,
+    HomeComponent,
   ],
   imports: [
     CommonModule,
