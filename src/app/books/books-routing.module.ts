37523c0a1f46fe070645f3fdcc3c99e32cbee2f6
eugenio <eugenio@baliotech.com>
2022-10-28 00:14:22 -0500

    not longer pinting to home component

--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
 import { IndexComponent } from './pages/index/index.component';
 import { WishListComponent } from './pages/wish-list/wish-list.component';
 
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent, children: [
+    path: '', children: [
       { path: 'list', component: IndexComponent },
       { path: 'wish-list', component: WishListComponent },
     ]
--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,10 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { MaterialModule } from '../material/material.module';
 import { BooksRoutingModule } from './books-routing.module';
-import { HomeComponent } from './pages/home/home.component';
 import { IndexComponent } from './pages/index/index.component';
 import { WishListComponent } from './pages/wish-list/wish-list.component';
 
@@ -13,13 +10,10 @@ import { WishListComponent } from './pages/wish-list/wish-list.component';
 @NgModule({
   declarations: [
     IndexComponent,
-    HomeComponent,
     WishListComponent,
   ],
   imports: [
     CommonModule,
-    MaterialModule,
-    FlexLayoutModule,
     BooksRoutingModule
   ]
 })
