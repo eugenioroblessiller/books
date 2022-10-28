ffd8bbd31b1eca099b0cff4578526e1a4cf2b9a9
eugenio <eugenio@baliotech.com>
2022-10-27 22:43:13 -0500

    material components

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
 @NgModule({
   declarations: [],
+  exports:[
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
+  ],
   imports: [
     CommonModule
   ]
