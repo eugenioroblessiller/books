1c6c6d5ec4d81c56d01b045eae667f6eb8b29992
eugenio <eugenio@baliotech.com>
2022-10-28 00:15:59 -0500

    book interface

--- src/app/books/interfaces/books.ts
+++ src/app/books/interfaces/books.ts
@@ -1,2 +1,12 @@
-export interface IBooks {
+export interface IBook {
+  isbn: string;
+  title: string;
+  isInWishList: boolean;
+  subtitle: string;
+  author: string;
+  published: Date;
+  publisher: string;
+  pages: number;
+  description: string;
+  website: string;
 }
