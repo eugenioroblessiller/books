56e9c825fd66216dd1e52cbf9b9d5a1b87e87a4a
eugenio <eugenio@baliotech.com>
2022-10-29 18:57:17 -0500

    canActivate working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './auth/guard/auth.guard';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
@@ -15,6 +16,7 @@ const routes: Routes = [
   },
   {
     path: '', component: AdminLayoutComponent,
+    canActivate:[AuthGuard],
     children: [
       { path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksModule) },
       { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
