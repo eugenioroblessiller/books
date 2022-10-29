5faa1bd4f5fb5fbfb40a7039cdd4c64401fd5f2d
eugenio <eugenio@baliotech.com>
2022-10-29 12:57:21 -0500

    new Material components

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,6 +5,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 
 
@@ -15,7 +17,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatToolbarModule,
     MatIconModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
   ],
   imports: [
     CommonModule
