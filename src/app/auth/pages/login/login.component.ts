d1bef4a657995ab19166d53c1358def1767c355a
eugenio <eugenio@baliotech.com>
2022-10-29 13:08:32 -0500

    ng model for login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/users/interfaces/user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user!: IUser;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.user = {
+      id: 0,
+      name: '',
+      email: '',
+      password: ''
+    }
   }
 
+
+
+  loginUser() {
+    console.log('login', this.user)
+  }
 }
