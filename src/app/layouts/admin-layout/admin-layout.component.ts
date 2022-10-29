2088ffcedae4c16b4461aec94dfd03d60100acbc
eugenio <eugenio@baliotech.com>
2022-10-29 18:57:09 -0500

    Logout button working

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-admin-layout',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _authService: AuthService,
+    private _modal: MatDialog,
+    private _router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+
+  askBeforeLogout() {
+    const message = "Are you sure you want to logout?"
+    const config: MatDialogConfig = { data: { message, title: "LOGOUT" }, width: '300px' }
+
+    const modal = this._modal.open(ConfirmDialogComponent, config);
+    modal.afterClosed().subscribe(confirmed => {
+      if (confirmed) { this.logout(); }
+    });
+  }
+
+  logout() {
+    this._authService.logout()
+    this._router.navigate(['auth/login'])
+  }
 }
