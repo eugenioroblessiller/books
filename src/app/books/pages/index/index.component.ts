03e0800355b61c7359365a4cfd7e0e7ffd074fab
eugenio <eugenio@baliotech.com>
2022-10-29 19:23:41 -0500

    showing books

--- src/app/books/pages/index/index.component.ts
+++ src/app/books/pages/index/index.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LoaderComponent } from 'src/app/shared/loader/loader.component';
+import { IBook } from '../../interfaces/books';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("booksLoader", { static: true }) loader!: LoaderComponent;
+
+  books: IBook[] = []
+
+  constructor(
+    private _booksService: BooksService
+  ) { }
 
   ngOnInit(): void {
+    this.getBooks()
+  }
+
+  async getBooks() {
+    this.loader.show()
+    try {
+      const res = await this._booksService.getBooks()
+      console.log(res)
+      this.books = res
+      this.loader.hide()
+    } catch (error) {
+      console.error(error)
+      this.loader.hide()
+    }
   }
 
 }
