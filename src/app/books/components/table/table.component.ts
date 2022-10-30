cdf7a8266c6c37fde4ce1fcf38b1b2c499b52c5f
eugenio <eugenio@baliotech.com>
2022-10-30 12:00:49 -0600

    logic in table component is working

--- src/app/books/components/table/table.component.ts
+++ src/app/books/components/table/table.component.ts
@@ -1,7 +1,14 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTable } from '@angular/material/table';
+import { showErrorDialog } from 'src/app/helpers/helpers';
+import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
+import { LoaderComponent } from 'src/app/shared/loader/loader.component';
 
 import { IBook } from '../../interfaces/books';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'book-table',
@@ -17,14 +24,63 @@ import { IBook } from '../../interfaces/books';
 })
 export class TableComponent implements OnInit {
 
+  @ViewChild("bookLoader", { static: true }) loader!: LoaderComponent;
+  @ViewChild("bookTable") table!: MatTable<IBook>;
+
   @Input() books!: IBook[]
   @Input() columnsToDisplay: any[] = []
   @Input() expandedElement: any
   @Input() columnsToDisplayWithExpand: any[] = []
 
-  constructor() { }
+  @Output() newBookEventEmitter = new EventEmitter<IBook>();
+  @Output() deleteBookEventEmitter = new EventEmitter<IBook>();
+
+  constructor(
+    private _booksService: BooksService,
+    private _modal: MatDialog,
+    private _snackbar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
   }
 
+
+  async toggleBookInWishList(book: IBook) {
+    book.isInWishList = !book.isInWishList
+    this.loader.show()
+    try {
+      const res = await this._booksService.updateBook(book)
+      console.log(res)
+      this.newBookEventEmitter.emit(res)
+      this.table.renderRows()
+      this.loader.hide()
+    } catch (error) {
+      console.error(error)
+      this.loader.hide()
+      showErrorDialog('Error', `There was an error, please try agin latter`, 'OK', this._modal);
+    }
+  }
+
+  askBeforeDelete(book: IBook) {
+    const message = `Are you sure you want to delete the book: ${book.title}?`
+    const config: MatDialogConfig = { data: { message, title: "DELETE BOOK", color: "warn" }, width: '300px' }
+
+    const modal = this._modal.open(ConfirmDialogComponent, config);
+    modal.afterClosed().subscribe(confirmed => {
+      if (confirmed) { this.deleteBook(book); }
+    });
+  }
+
+  async deleteBook(book: IBook) {
+    this.loader.show()
+    try {
+      const res = await this._booksService.deleteBook(book)
+      this.deleteBookEventEmitter.emit(res)
+      this.loader.hide()
+    } catch (error) {
+      console.error(error)
+      this.loader.hide()
+      showErrorDialog("Error", "Something went wrong, we couldn't delete the book", "OK", this._modal)
+    }
+  }
 }
