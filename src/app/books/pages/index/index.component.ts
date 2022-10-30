64c7aa6370d5b61e2eba17fe488acf0c7488e802
eugenio <eugenio@baliotech.com>
2022-10-29 20:29:33 -0500

    receiving the emitter and removing the book

--- src/app/books/pages/index/index.component.ts
+++ src/app/books/pages/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { LoaderComponent } from 'src/app/shared/loader/loader.component';
+
 import { IBook } from '../../interfaces/books';
 import { BooksService } from '../../services/books.service';
 
@@ -27,12 +28,17 @@ export class IndexComponent implements OnInit {
     try {
       const res = await this._booksService.getBooks()
       console.log(res)
-      this.books = res
+      this.books = res.filter(book => book.isInWishList == false)
       this.loader.hide()
     } catch (error) {
       console.error(error)
       this.loader.hide()
     }
+  }
+
+  bookEdited(book: IBook) {
+    const index = this.books.findIndex(book => book.id == book.id)
+    this.books.splice(index, 1)
   }
 
 }
