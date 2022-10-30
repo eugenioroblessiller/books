ffe0568d84e225d64b4c3ccd29c7e11ad66566ed
eugenio <eugenio@baliotech.com>
2022-10-29 20:28:04 -0500

    showing books in whislist

--- src/app/books/pages/wish-list/wish-list.component.ts
+++ src/app/books/pages/wish-list/wish-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LoaderComponent } from 'src/app/shared/loader/loader.component';
+
+import { IBook } from '../../interfaces/books';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-wish-list',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishListComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("whislistLoader", { static: true }) loader!: LoaderComponent;
+
+  books: IBook[] = []
+
+  constructor(
+    private _booksService: BooksService
+  ) { }
 
   ngOnInit(): void {
+    this.getBooks()
   }
 
+
+  async getBooks() {
+    this.loader.show()
+    try {
+      const res = await this._booksService.getBooks()
+      console.log(res)
+      this.books = res.filter(book => book.isInWishList == true)
+      this.loader.hide()
+    } catch (error) {
+      console.error(error)
+      this.loader.hide()
+    }
+  }
+
+
+  bookEdited(book: IBook) {
+    const index = this.books.findIndex(book => book.id == book.id)
+    this.books.splice(index, 1)
+  }
 }
