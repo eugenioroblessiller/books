c4456abc49f25437d10c9f8e159a5c54c3618326
eugenio <eugenio@baliotech.com>
2022-10-29 19:19:30 -0500

    import FlexLayoutModule

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,6 +1,8 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
 import { BooksRoutingModule } from './books-routing.module';
 import { IndexComponent } from './pages/index/index.component';
@@ -16,7 +18,9 @@ import { WishListComponent } from './pages/wish-list/wish-list.component';
   imports: [
     CommonModule,
     BooksRoutingModule,
-    SharedModule
+    SharedModule,
+    MaterialModule,
+    FlexLayoutModule
   ]
 })
 export class BooksModule { }
