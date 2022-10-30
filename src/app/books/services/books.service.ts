097484a6ede0a4743a308bda373814080f9f0ba9
eugenio <eugenio@baliotech.com>
2022-10-29 19:23:32 -0500

    service for books

--- src/app/books/services/books.service.ts
+++ src/app/books/services/books.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { IBook } from '../interfaces/books';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  constructor() { }
+  private baseUrl: string = environment.baseUrl;
+
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  getBooks() {
+    const url = `${this.baseUrl}/books`
+    return lastValueFrom(this._http.get<IBook[]>(url))
+  }
+
+  getWishList() {
+    const url = `${this.baseUrl}/books`
+    return lastValueFrom(this._http.get<IBook[]>(url))
+  }
+
+  createBook() {
+
+  }
+
+  readBook() {
+
+  }
+
+  updateBook() {
+
+  }
+
+  deleteBook() {
+
+  }
 }
