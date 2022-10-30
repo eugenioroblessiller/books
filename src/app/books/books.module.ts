0bb88e153ecb03c45e28cba890f4f9e1f91836e5
eugenio <eugenio@baliotech.com>
2022-10-29 19:54:19 -0500

    beuatify

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,13 +1,13 @@
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
 import { BooksRoutingModule } from './books-routing.module';
+import { BookComponent } from './components/book/book.component';
 import { IndexComponent } from './pages/index/index.component';
 import { WishListComponent } from './pages/wish-list/wish-list.component';
-import { BookComponent } from './components/book/book.component';
 
 
 
